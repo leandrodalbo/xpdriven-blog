242fa1080be932e6cb17980edb33518e1a2aff77
leandrodalbo <leandro.edbo@gmail.com>
2025-02-21 19:36:03 +0000

    adding youtube channel

--- src/blog-header/BlogHeader.tsx
+++ src/blog-header/BlogHeader.tsx
@@ -1,39 +1,45 @@
-import { FaGithub, FaLinkedin, FaSyncAlt } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaSyncAlt, FaYoutube } from "react-icons/fa";
 
 const BlogHeader = () => {
-    return (
-        <header className="bg-gray-800 p-4 flex justify-between items-center shadow-md border-b border-yellow-400">
+  return (
+    <header className="bg-gray-800 p-4 flex justify-between items-center shadow-md border-b border-yellow-400">
+      <a
+        href="/"
+        className="flex space-x-6 text-2xl md:text-3xl text-yellow-300 hover:text-orange-400 font-bold transition duration-300"
+      >
+        <FaSyncAlt className="animate-spin-color" />
+        <span>XPDriven Blog</span>
+      </a>
 
-            <a
-                href="/"
-                className="flex space-x-6 text-2xl md:text-3xl text-yellow-300 hover:text-orange-400 font-bold transition duration-300"
+      <nav className="flex space-x-6">
+        <a
+          href="https://www.youtube.com/@xpdrivenblog"
+          className="text-pink-300 hover:text-yellow-300 transform hover:scale-110 transition duration-300 text-2xl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaYoutube />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/leandrodlb/"
+          className="text-pink-300 hover:text-yellow-300 transform hover:scale-110 transition duration-300 text-2xl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
 
-            >
-                <FaSyncAlt className="animate-spin-color" />
-                <span>XPDriven Blog</span>
-            </a>
-
-            <nav className="flex space-x-6">
-                <a
-                    href="https://www.linkedin.com/in/leandrodlb/"
-                    className="text-pink-300 hover:text-yellow-300 transform hover:scale-110 transition duration-300 text-2xl"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <FaLinkedin />
-                </a>
-
-                <a
-                    href="https://github.com/leandrodalbo"
-                    className="text-pink-300 hover:text-yellow-300 transform hover:scale-110 transition duration-300 text-2xl"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <FaGithub />
-                </a>
-            </nav>
-        </header>
-    );
+        <a
+          href="https://github.com/leandrodalbo"
+          className="text-pink-300 hover:text-yellow-300 transform hover:scale-110 transition duration-300 text-2xl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default BlogHeader;
