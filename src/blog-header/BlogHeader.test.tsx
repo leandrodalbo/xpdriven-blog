8a5bb2b62fa46eac2dddb6eef7c88fd04865d39d
leandrod <leandro.edbo@gmail.com>
2025-02-21 19:37:01 +0000

    adding youtube channel (#7)

--- src/blog-header/BlogHeader.test.tsx
+++ src/blog-header/BlogHeader.test.tsx
@@ -3,23 +3,43 @@ import { describe, it, expect } from "vitest";
 import BlogHeader from "../blog-header/BlogHeader";
 
 describe("BlogHeader Tests", () => {
-    it("renders the blog title", () => {
-        render(<BlogHeader />);
-        expect(screen.getByText("XPDriven Blog")).toBeInTheDocument();
-    });
+  it("renders the blog title", () => {
+    render(<BlogHeader />);
+    expect(screen.getByText("XPDriven Blog")).toBeInTheDocument();
+  });
 
+  it("contains a link to the homepage", () => {
+    render(<BlogHeader />);
+    expect(
+      screen.getByRole("link", { name: /XPDriven Blog/i })
+    ).toHaveAttribute("href", "/");
+  });
 
-    it("contains a link to the homepage", () => {
-        render(<BlogHeader />);
-        expect(screen.getByRole("link", { name: /XPDriven Blog/i })).toHaveAttribute("href", "/");
-    });
+  it("renders the links to Linkedin and Github", () => {
+    render(<BlogHeader />);
 
-    it("renders the links to Linkedin and Github", () => {
-        render(<BlogHeader />);
+    const links = screen.getAllByRole("link");
 
-        const links = screen.getAllByRole("link");
+    expect(
+      links.some(
+        (link) =>
+          link.getAttribute("href") === "https://www.youtube.com/@xpdrivenblog"
+      )
+    ).toBe(true);
 
-        expect(links.some(link => link.getAttribute("href") === "https://github.com/leandrodalbo")).toBe(true);
-        expect(links.some(link => link.getAttribute("href") === "https://www.linkedin.com/in/leandrodlb/")).toBe(true);
-    });
+    expect(
+      links.some(
+        (link) =>
+          link.getAttribute("href") === "https://github.com/leandrodalbo"
+      )
+    ).toBe(true);
+
+    expect(
+      links.some(
+        (link) =>
+          link.getAttribute("href") ===
+          "https://www.linkedin.com/in/leandrodlb/"
+      )
+    ).toBe(true);
+  });
 });
