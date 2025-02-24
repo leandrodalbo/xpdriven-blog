79490ad05a82292a42ca99b8860dd9d0aa7f1321
leandrod <leandro.edbo@gmail.com>
2025-02-24 19:33:58 +0000

    coding monkeys article (#14)

--- src/article-selector/ArticleSelector.tsx
+++ src/article-selector/ArticleSelector.tsx
@@ -2,12 +2,14 @@ import Intro from "../blog-article/Intro";
 import WhatIsCleanCode from "../blog-article/WhatIsCleanCode";
 import CodingMonkeys from "../blog-article/CodingMonkeys";
 import WhatIsTDD from "../blog-article/WhatIsTDD";
+import AgileManifesto from "../blog-article/AgileManifesto";
 
 const articleSelector: Record<number, () => JSX.Element> = {
   0: () => <CodingMonkeys />,
   1: () => <Intro />,
   2: () => <WhatIsTDD />,
   3: () => <WhatIsCleanCode />,
+  4: () => <AgileManifesto />,
 };
 
 export default articleSelector;
---
+++ src/blog-article/AgileManifesto.tsx
@@ -0,0 +1,13 @@
+import monkeys from "../assets/monkeyeating.gif";
+
+const AgileManifesto = () => (
+  <article className="blog-article">
+    <h1 className="blog-article-h1">Agile Manifesto: What It Means Today</h1>
+
+    <p className="blog-article-p">This article is just about to be added...</p>
+
+    <img src={monkeys} alt="coding-monkeys" className="blog-article-img" />
+  </article>
+);
+
+export default AgileManifesto;
