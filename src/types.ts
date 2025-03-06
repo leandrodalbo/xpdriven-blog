60651bc3a6160345182caeaaa628e4bd3731aa47
leandrod <leandro.edbo@gmail.com>
2025-03-06 15:43:58 +0000

    articles refactoring: reusable Article header component (#18)

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,16 @@
 export interface BlogArticle {
-    id: number,
-    title: string,
+  id: number;
+  title: string;
 }
 
+export interface ArticleHeaderData {
+  h1: string;
+  p1: string;
+  h2?: string;
+  img?: string;
+  p2?: string;
+}
 
+export interface ArticleData {
+  headerData: ArticleHeaderData;
+}
