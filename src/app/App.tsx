46d8dee3d10037f176291ff3241bc5c41e205010
leandrodalbo <leandro.edbo@gmail.com>
2025-03-15 13:01:57 +0000

    adding X + refactoring the blog header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import ArticlePage from "../article-page/ArticlePage";
 import BlogFooter from "../blog-footer/BlogFooter";
 import { articles } from "../blog-articles";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { headerContent } from "../general-content/header-content";
 
 export const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-[#121826]">
-        <BlogHeader />
+        <BlogHeader data={headerContent} />
         <div className="flex flex-1">
           <BlogSidebar articles={articles} />
           <main className="flex-1 p-6">
