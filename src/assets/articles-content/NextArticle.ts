30942126193c18358062161da4cd284201aebb65
leandrodalbo <leandro.edbo@gmail.com>
2025-03-22 11:32:07 +0000

    scrum/kanban article

--- src/assets/articles-content/NextArticle.ts
+++ src/assets/articles-content/NextArticle.ts
@@ -2,7 +2,7 @@ import { ArticleData } from "../../types/types";
 
 export const nextArticleContent: ArticleData = {
   headerData: {
-    h1: "Next Article: Scrum vs. Kanban",
+    h1: "How to Stay Productive as a Developer",
     p1: "This article is just about to be added...",
     p1class: "blog-article-p-center",
   },
