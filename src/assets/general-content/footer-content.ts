c0d7a8e5a42365f5ef5cd4372c6d78cc0329bea2
leandrod <leandro.edbo@gmail.com>
2025-04-28 20:17:59 -0300

    removing youtube channel (#44)

--- src/assets/general-content/footer-content.ts
+++ src/assets/general-content/footer-content.ts
@@ -1,11 +1,10 @@
 import { BlogFooterContent } from "../../types/types";
-import { FaGithub, FaYoutube } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { FaX } from "react-icons/fa6";
 
 export const footerContent: BlogFooterContent = {
   text: `© ${new Date().getFullYear()} XPDriven Blog. ❤️`,
   socialLinks: [
-    { url: "https://www.youtube.com/@xpdrivenblog", socialIcon: FaYoutube },
     { url: "https://x.com/xpdrivenblog", socialIcon: FaX },
     { url: "https://github.com/leandrodalbo", socialIcon: FaGithub },
   ],
